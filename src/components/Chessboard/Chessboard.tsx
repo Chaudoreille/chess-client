30505015dad759caf7f735c3585d73b03bbd1259
Chaudoreille <a.chaudoreille@gmail.com>
2023-05-12 00:39:04 +0200

    add chess pieces and click handler

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -10,11 +10,17 @@ enum PieceType {
   pawn
 }
 
-type Piece = {
-  color: string,
-  type: PieceType,
-  moves: number[]
-} | undefined;
+class Piece {
+  color: string;
+  type: PieceType;
+  moves: number[];
+
+  constructor(color:string, type:PieceType, legalMoves:number[]) {
+    this.color = color;
+    this.type = type;
+    this.moves = legalMoves;
+  }
+}
 
 enum Color {
   white= "white",
@@ -31,21 +37,15 @@ function squareColor(index: number): string {
 
 function algabreicNotation(index: number): string {
   const aCode = 97;
-  const column: string = String.fromCharCode(aCode + 7 - Math.floor(index / 8));
-  const row: number = (index % 8) + 1;
+  const column: string = String.fromCharCode(aCode + (index % 8));
+  const row: number = 7 - Math.floor(index / 8) + 1;
 
   return `${column}${row}`;
 }
 
-const startingBoard = (): Piece[] => {
-  const board = Array<Piece>(64).fill(undefined);
-  const insertRow = (board: Piece[], row: PieceType[], color:Color, start:number) => {
-    board.splice(start, row.length, ...row.map(type => ({
-      color,
-      type,
-      moves: []
-    })))
-  };
+const startingBoard = (): Array<Piece|undefined> => {
+  const board = Array<Piece|undefined>(64).fill(undefined);
+  const pawnRow = Array<PieceType>(8).fill(PieceType.pawn);
   const backRow = [
     PieceType.rook,
     PieceType.knight,
@@ -56,7 +56,11 @@ const startingBoard = (): Piece[] => {
     PieceType.knight,
     PieceType.rook
   ];
-  const pawnRow = Array<PieceType>(8).fill(PieceType.pawn);
+
+  const insertRow = (board: Array<Piece|undefined>, row: PieceType[], color:Color, start:number) => {
+    board.splice(start, row.length, ...row.map(type => new Piece(color, type, [])));
+  };
+
   insertRow(board, backRow, Color.black, 0);
   insertRow(board, pawnRow, Color.black, 8);
   insertRow(board, pawnRow, Color.white, 48);
@@ -66,11 +70,48 @@ const startingBoard = (): Piece[] => {
 
 export const Chessboard = () => {
   const [squares, setSquares] = useState(startingBoard());
+  const [selectedPieceIndex, setSelectedPieceIndex] = useState(-1);
+
+  const handleClick = (event: Event) => {
+    const target = event.currentTarget;
+
+    if (target instanceof Element && target.hasAttribute("data-index")) {
+      const targetIndex = Number(target.getAttribute("data-index"));
+
+      console.log(selectedPieceIndex, algabreicNotation(selectedPieceIndex));
+      console.log(targetIndex, algabreicNotation(targetIndex));
+      console.log(squares)
+      console.log(squares.length)
+
+      if (selectedPieceIndex === -1) {
+        if (squares[targetIndex] instanceof Piece) {
+          setSelectedPieceIndex(targetIndex);
+        } else {
+          setSelectedPieceIndex(-1);
+        }
+        return;
+      }
+
+      movePiece(selectedPieceIndex, targetIndex);
+      setSelectedPieceIndex(-1);
+      return
+    }
+  }
+
+  const movePiece = (origin:number, destination:number) => {
+    setSquares(old => {
+      const update = [...old];
+      update[destination] = update[origin];
+      update[origin] = undefined;
+
+      return update;
+    })
+  }
 
   return (
     <div className='Chessboard'>
       {squares.map((piece, index) => (
-        <div key={algabreicNotation(index)} className={`square ${squareColor(index)} ${algabreicNotation(index)}`}>
+        <div key={algabreicNotation(index)} className={`square ${squareColor(index)}`} data-index={index} onClick={handleClick}>
           {piece && <div className={`piece ${piece.color} ${PieceType[piece.type]}`}></div> }
         </div>
       ))}
