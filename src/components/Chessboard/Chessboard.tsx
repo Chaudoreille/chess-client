e191c28f6c5ed4aff4a551f64a73af919334efc9
Chaudoreille <a.chaudoreille@gmail.com>
2023-05-12 13:33:12 +0200

    add Square class to make code more readable

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -1,4 +1,4 @@
-import { useState} from 'react'
+import { useState, MouseEvent} from 'react'
 import './Chessboard.css'
 
 enum PieceType {
@@ -8,6 +8,11 @@ enum PieceType {
   knight,
   rook,
   pawn
+}
+
+enum Color {
+  white= "white",
+  black= "black",
 }
 
 class Piece {
@@ -20,11 +25,31 @@ class Piece {
     this.type = type;
     this.moves = legalMoves;
   }
+
+  toString() {
+    return `${this.color[0]}${PieceType[this.type][0]}`;
+  }
 }
 
-enum Color {
-  white= "white",
-  black= "black",
+class Square {
+  piece: Piece | null;
+
+  constructor(piece:Piece | null = null) {
+    this.piece = piece;
+  }
+
+  isEmpty() {
+    return this.piece === null;
+  }
+
+  empty() {
+    const removed = this.piece;
+    this.piece = null;
+    return removed;
+  }
+  toString() {
+    return this.piece?.toString() || "-";
+  }
 }
 
 function squareColor(index: number): string {
@@ -43,8 +68,8 @@ function algabreicNotation(index: number): string {
   return `${column}${row}`;
 }
 
-const startingBoard = (): Array<Piece|undefined> => {
-  const board = Array<Piece|undefined>(64).fill(undefined);
+const startingBoard = (): Square[] => {
+  const board = Array(64).fill(0).map(() => new Square());
   const pawnRow = Array<PieceType>(8).fill(PieceType.pawn);
   const backRow = [
     PieceType.rook,
@@ -57,62 +82,59 @@ const startingBoard = (): Array<Piece|undefined> => {
     PieceType.rook
   ];
 
-  const insertRow = (board: Array<Piece|undefined>, row: PieceType[], color:Color, start:number) => {
-    board.splice(start, row.length, ...row.map(type => new Piece(color, type, [])));
+  const insertRow = (board: Square[], row: PieceType[], color:Color, start:number) => {
+    row.forEach((type, index) => {
+      board[start + index].piece = new Piece(color, type, [])
+    });
   };
 
   insertRow(board, backRow, Color.black, 0);
   insertRow(board, pawnRow, Color.black, 8);
   insertRow(board, pawnRow, Color.white, 48);
   insertRow(board, backRow, Color.white, 56);
+
   return board;
 }
 
 export const Chessboard = () => {
   const [squares, setSquares] = useState(startingBoard());
-  const [selectedPieceIndex, setSelectedPieceIndex] = useState(-1);
+  const [selectedPieceIndex, setSelectedPieceIndex] = useState(-1); // selectedPieceIndex is -1 if no piece is currently selected.
+  const movePiece = (originIndex:number, destinationIndex:number) => {
+    const update = [...squares];
+    const movedPiece  = update[originIndex].empty();
+    update[destinationIndex].piece = movedPiece;
 
-  const handleClick = (event: Event) => {
-    const target = event.currentTarget;
-
-    if (target instanceof Element && target.hasAttribute("data-index")) {
-      const targetIndex = Number(target.getAttribute("data-index"));
-
-      console.log(selectedPieceIndex, algabreicNotation(selectedPieceIndex));
-      console.log(targetIndex, algabreicNotation(targetIndex));
-      console.log(squares)
-      console.log(squares.length)
-
-      if (selectedPieceIndex === -1) {
-        if (squares[targetIndex] instanceof Piece) {
-          setSelectedPieceIndex(targetIndex);
-        } else {
-          setSelectedPieceIndex(-1);
-        }
-        return;
-      }
-
-      movePiece(selectedPieceIndex, targetIndex);
-      setSelectedPieceIndex(-1);
-      return
-    }
+    setSquares(update);
   }
 
-  const movePiece = (origin:number, destination:number) => {
-    setSquares(old => {
-      const update = [...old];
-      update[destination] = update[origin];
-      update[origin] = undefined;
+  const handleSquareClicked = (event: MouseEvent<HTMLElement>) => {
+    const indexAttribute = event?.currentTarget?.dataset?.index;
+    if (typeof indexAttribute !== "string") {
+      throw new Error(`${event.currentTarget} does not have an index property`);
+    }
 
-      return update;
-    })
+    const targetIndex = Number(indexAttribute);
+
+    if (targetIndex < 0 || targetIndex >= squares.length) {
+      throw new Error(`${targetIndex} is not a valid square index`);
+    } else {
+      if (selectedPieceIndex === -1) { // no piece selected
+        if (!squares[targetIndex].isEmpty()) { 
+          setSelectedPieceIndex(targetIndex);
+        }
+        return;
+      } else { // a piece is selected
+        movePiece(selectedPieceIndex, targetIndex);
+        setSelectedPieceIndex(-1);
+      }
+    }
   }
 
   return (
     <div className='Chessboard'>
-      {squares.map((piece, index) => (
-        <div key={algabreicNotation(index)} className={`square ${squareColor(index)}`} data-index={index} onClick={handleClick}>
-          {piece && <div className={`piece ${piece.color} ${PieceType[piece.type]}`}></div> }
+      {squares.map((square, index) => (
+        <div key={algabreicNotation(index)} className={`square ${squareColor(index)}`} data-index={index} onClick={handleSquareClicked}>
+          {square.piece !== null && <div className={`piece ${square.piece.color} ${PieceType[square.piece.type]}`}></div> }
         </div>
       ))}
     </div>
